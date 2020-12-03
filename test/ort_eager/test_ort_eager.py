f5a3866bdb6bf3aab409a6dd53ddf394e636d857
Cheng Tang <souptc@gmail.com>
2020-12-03 18:52:34 +0000

    support copy ort tensor to pytorch cpu

--- test/ort_eager/test_ort_eager.py
+++ test/ort_eager/test_ort_eager.py
@@ -9,8 +9,10 @@ class TestCustomOps(unittest.TestCase):
     def test_basic(self):
         device = torch.device("ort")
         x = torch.empty(5, 3, device = device)
-        #TODO: enable the test later
-        #assert(x is None)
-
+        y = torch.empty(5, 3, device = device)
+        z = x + y
+        z_pt = x.cpu() + y.cpu()
+        assert torch.allclose(z.cpu(), z_pt)
+    
 if __name__ == '__main__':
     unittest.main()
