531757120d497ccf8f88a483ab7f0c922358da30
Chandru Ramakrishnan <chandru-r@github.com>
2021-03-11 14:04:06 -0500

    Converted tabs to spaces.

--- ort_eager_build.sh
+++ ort_eager_build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 time (
-	USE_ORT=1 \
-	USE_CUDA=0 \
-	USE_MKLDNN=0 \
-	USE_OPENMP=0 \
-	MACOSX_DEPLOYMENT_TARGET=10.9 \
-	CC=clang \
-	CXX=clang++ \
-	python setup.py install
+    USE_ORT=1 \
+    USE_CUDA=0 \
+    USE_MKLDNN=0 \
+    USE_OPENMP=0 \
+    MACOSX_DEPLOYMENT_TARGET=10.9 \
+    CC=clang \
+    CXX=clang++ \
+    python setup.py install
 )
-
